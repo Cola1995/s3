c973d391c3b5ead068fb0f54ef35a0af3f265155
Cola1995 <991571566@qq.com>
2019-01-09 17:43:19 +0800

    day28

---
+++ 面向对象/testss.py
@@ -0,0 +1,9 @@
+st='ss'
+s1='ss2'
+print(type(st))
+print(type(s1))
+if type(st) is not type(s1):
+    print('ok')
+
+else:
+    print('sb')
--- 面向对象/描述符应用.py
+++ 面向对象/描述符应用.py
@@ -1,7 +1,8 @@
 class typed:
 
-    def __init__(self,key):
+    def __init__(self,key,type):
         self.key=key
+        self.type=type
 
     def __get__(self, instance, owner):
         print('get方法')
@@ -10,11 +11,12 @@ class typed:
 
     def __set__(self, instance, value):
         print('set方法')
-        print(instance,value)
-        if instance is not str:
+        print(type(instance),type(value),self.type)
+        if type(value) is not self.type:
+        # if not isinstance(value, self.type):
             raise TypeError('传入数据类型错误')
             #print('传入数据类型错误')
-            return
+            #return
         instance.__dict__[self.key]=value
         # print(instance.__dict__[self.key])
     def __delete__(self, instance):
@@ -25,12 +27,13 @@ class typed:
 
 
 class aa:
-    name=typed('name')
-    def __init__(self,name,age):
+    name=typed('name',str)
+    def __init__(self,name,age,gongzi):
         self.name=name
         self.age=age
+        self.gongzi=gongzi
 
-a1=aa(12,10)
+a1=aa('oo',10,12.0)
 #del a1.name
 print(a1.__dict__)
 
